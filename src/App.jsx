9f62acd5605f602e2ee9e39010913a817bc196a0
Anas Alam <anasalam.xyz@gmail.com>
2026-04-30 11:54:36 +0530

    vercel: analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import './App.css'
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Footer from './components/Footer';
+import { Analytics } from "@vercel/analytics/next";
 
 function App() {
   return(
     <>
+      <Analytics/>
       <Navbar/>
       <Home/>
       <Footer/>
