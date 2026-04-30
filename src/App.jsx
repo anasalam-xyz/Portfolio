09d9a78404020af7ffcee18dd1242b3d009c0dd4
Anas Alam <anasalam.xyz@gmail.com>
2026-04-30 12:16:41 +0530

    vercel: speed-analytics

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Footer from './components/Footer';
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
   return(
     <>
       <Analytics/>
+      <SpeedInsights/>
       <Navbar/>
       <Home/>
       <Footer/>
