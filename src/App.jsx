6bd1c802a507c608316e14aff49ca6022a4ff8ed
Anas Alam <anasalam.xyz@gmail.com>
2026-04-30 12:01:58 +0530

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Footer from './components/Footer';
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return(
